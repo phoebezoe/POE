92e83534d6c7712e1983ce1f4c44eeaf9a45a2b0
Student <Student@LabVM2049939.jx22p0luidyundktxoea3w3lbb.jnbx.internal.cloudapp.net>
2026-09-18 00:10:42 +0200

    third

--- src/main/java/com/mycompany/part1/Login.java
+++ src/main/java/com/mycompany/part1/Login.java
@@ -3,11 +3,37 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.mycompany.part1;
-
+import java.util.regex.Pattern;
 /**
  *
  * @author Student
  */
 public class Login {
-    
+    private String username;
+    private String password;
+    private String cellphoneNumber;
+    private String firstName;
+    private String lastName;
+
+  
+    public Login(String username, String cellphoneNumber, String password,
+                 String firstName, String lastName) {
+        this.username = username;
+        this.cellphoneNumber = cellphoneNumber;
+        this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public boolean checkUserName(String username) {
+        boolean Username;
+        if (username.contains("_") && username.length() <= 5){
+        Username = true;
+        System.out.println("Username successfully captured.");
+    }else{
+        Username = false;
+       System.out.println("Username is not correctly formatted; please ensure that your username contains an underscore and is no more than five characters in length.");
+            }
+        return Username;
+    }
 }
