27d97490631034f9fb74936fde16e340f991bb83
Student <Student@LabVM2049939.jx22p0luidyundktxoea3w3lbb.jnbx.internal.cloudapp.net>
2026-09-17 23:56:15 +0200

    second

--- src/main/java/com/mycompany/part1/Part1.java
+++ src/main/java/com/mycompany/part1/Part1.java
@@ -24,5 +24,24 @@ public class Part1 {
         String password = input.nextLine();
         System.out.print("Please enter a phone  number (e.g. +27838968976):");
         String cellphoneNumber = input.nextLine(); 
+        
+       
+         Login login = new Login(username, cellphoneNumber, password, firstName,lastName);
+
+        String registrationResult = login.registerUser(username, password);
+        System.out.println(registrationResult);
+
+        if (registrationResult.equals("Username and password successfully captured.")) {
+            System.out.print("Please re-enter your username: ");
+            String enteredUsername = input.nextLine();
+            System.out.print("Please re-enter your password: ");
+            String enteredPassword = input.nextLine();
+
+          
+            System.out.println(login.returnLoginStatus(username, password, enteredUsername, enteredPassword, firstName, lastName));
+        }
+
+        input.close();
     }
 }
+
