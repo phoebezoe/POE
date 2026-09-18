e2b60a019eb013aeaf221d2a942457e4c6a60dd8
Student <Student@LabVM2049939.jx22p0luidyundktxoea3w3lbb.jnbx.internal.cloudapp.net>
2026-09-18 02:00:29 +0200

    fourth

--- src/main/java/com/mycompany/part1/Login.java
+++ src/main/java/com/mycompany/part1/Login.java
@@ -36,4 +36,62 @@ public class Login {
             }
         return Username;
     }
+     public boolean checkPasswordComplexity(String password) {
+        boolean Password;
+      if( password.matches ("^(?=.*[A-Z])(?=.*[0-9])(?=.*[^a-zA-Z0-9]).{8,}$")){
+      Password = true;
+          System.out.println("Password successfully captured.");
+    }else{
+      Password = false;
+          System.out.println("Password is not correctly formatted; please ensure that the password contains at least eight characters, a capital letter, a number, and a special character.");
+      }
+      return Password;
+    }
+
+    public boolean checkCellPhoneNumber(String cellphoneNumber) {
+        boolean cellphone;
+        String regexPattern = "^(\\+27)[6-8][0-9]{8}$";
+        
+        if (cellphoneNumber.matches(regexPattern)){
+            cellphone = true;
+            System.out.println("Cell number successfully captured.");
+        }else{
+            cellphone =  false;
+            System.out.println("Cell number is incorrectly formatted or does not contain an international code; please correct the number and try again.");
+        }
+        return cellphone;
+    }
+    public String registerUser(String username, String password){
+        
+        if(!checkUserName(username)){
+            return "The username is incorrectly formatted.";
+        }
+        if(!checkPasswordComplexity(password)){
+            return "The password does not meet the complexity requirements";
+    }
+      
+        return "Username and password successfully captured.";
+    }
+     public boolean loginUser(String username, String password, String enteredUsername, String enteredPassword) {
+        boolean logininformation;
+        
+        if(enteredUsername.equals(username) && enteredPassword.equals(password)){
+         logininformation = true;
+        System.out.println("A successful login");
+    }else{
+        logininformation = false;
+            System.out.println("A failed login");
+}
+    return logininformation;
+}
+
+    public String returnLoginStatus(String username, String password, String enteredUsername, String enteredPassword, String firstName, String lastName) {
+        
+        if(loginUser(username, password, enteredUsername, enteredPassword)){
+        
+            return "Welcome " + firstName + ", " + lastName + " it is great to see you again.";
+        }else{
+            return "Username or password incorrect, please try again.";
+    }
+}
 }
